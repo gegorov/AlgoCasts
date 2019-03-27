c2e2495b0c7fd662037ff870286e0c980739af38
gegorov <gegorov@yahoo.com>
2019-03-28 00:57:14 +0300

    feat: add reverseint solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const result = parseInt(n.toString().split('').reverse().join(''), 10);
+  return n >= 0 ? result : -result;
+}
+
+// function reverseInt(n) {
+
+//   return Math.sign(n) * parseInt(n.toString().split('').reverse().join(''), 10);
+// }
 
 module.exports = reverseInt;
