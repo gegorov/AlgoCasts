73cc6c08fd0d2f14c4b5b219338fbbf673cf0c59
gegorov <gegorov@yahoo.com>
2019-03-28 11:55:06 +0300

    feat: add maxchar solutions

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,43 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//   const result = {};
+//   let max = 0;
+//   let maxChar = '';
+//   str.split('').forEach((element) => {
+//     if (!result[element]) {
+//       result[element] = 1;
+//     } else {
+//       result[element] += 1;
+//     }
+//   });
+
+//   for (const char in result) {
+//     if (result[char] > max) {
+//       max = result[char];
+//       maxChar = char;
+//     }
+//   }
+
+//   return maxChar;
+// }
+
+function maxChar(str) {
+  const result = {};
+  str.split('').forEach((element) => {
+    if (!result[element]) {
+      result[element] = 1;
+    } else {
+      result[element] += 1;
+    }
+  });
+
+
+  const maxValue = Math.max(...Object.values(result));
+  return Object.keys(result).find(key => result[key] === maxValue);
+}
+
+maxChar('abcccccccd');
 
 module.exports = maxChar;
