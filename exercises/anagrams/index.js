159740f6aba85d277f2f12447f4621ed027d9d45
gegorov <gegorov@yahoo.com>
2019-03-29 00:35:50 +0300

    feat: add anagrams solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//   const normalize = string => string.replace(/[\W]/g, '').toLowerCase();
+//   const buildCharMap = (string) => {
+//     const charMap = {};
+//     string.split('').forEach((char) => {
+//       if (!string[char]) {
+//         charMap[char] = 1;
+//       } else {
+//         charMap[char] += 1;
+//       }
+//     });
+//     return charMap;
+//   };
+
+//   const normA = normalize(stringA);
+//   const normB = normalize(stringB);
+
+//   if (normA.length !== normB.length) {
+//     return false;
+//   }
+//   const sortedCharsA = Object.keys(buildCharMap(normA)).sort();
+//   const sortedCharsB = Object.keys(buildCharMap(normB)).sort();
+
+//   return sortedCharsA.every((value, index) => value === sortedCharsB[index]);
+// }
+
+function anagrams(stringA, stringB) {
+  const superNormalize = string => string
+    .replace(/[\W]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+  return superNormalize(stringA) === superNormalize(stringB);
+}
 
 module.exports = anagrams;
