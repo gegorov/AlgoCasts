4c27d7c18e48636b4cca59da1fc78573dee4a59c
gegorov <gegorov@yahoo.com>
2019-04-01 17:14:11 +0300

    feat: add steps solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,52 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   const makeLine = (lineNr, lengthLimit) => {
+//     let result = '';
+//     for (let i = 0; i <= lineNr; i += 1) {
+//       result += '#';
+//     }
+
+//     for (let j = 0; j < lengthLimit - lineNr - 1; j += 1) {
+//       result += ' ';
+//     }
+//     return result;
+//   };
+
+//   for (let i = 0; i < n; i += 1) {
+//     console.log(makeLine(i, n));
+//   }
+// }
+
+
+// function steps(n) {
+//   for (let row = 0; row < n; row += 1) {
+//     let stair = '';
+//     for (let column = 0; column < n; column += 1) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
+
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? '#' : ' ';
+
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
